d46df9f853344de5614ae4e00379d20d4ec42571
Eric Noll <eanoll88@gmail.com>
2020-02-03 20:53:08 -0600

    api route

--- routes/html.js
+++ routes/html.js
@@ -1,11 +1,59 @@
-const router = require('express').Router();
-const pat = require('path');
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+router.post("/api/workouts", (req, res) => {
+    console.log("route: post/api/workouts");
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
 
-// route for exercise.html
-router.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    console.log("route: put/api/workouts/" + params.id);
+    Workout.findByIdAndUpdate(
+        params.id,
+        { $push: { exercises: body } },
+        { new: true, runValidators: true }
+    )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
-/* route for stats.html */
-router.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/stats.html'));
+router.get("/api/workouts", (req, res) => {
+    console.log("route: get/api/workouts");
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
 });
+router.get("/api/workouts/range", (req, res) => {
+    console.log("route: get/api/workouts/range")
+    Workout.find({}).limit(7)
+        .then(dbWorkouts => {
+            console.log(dbWorkouts)
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+router.delete("/api/workouts", ({ body }, res) => {
+    console.log("route: delete/api/workouts")
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+module.exports = router;

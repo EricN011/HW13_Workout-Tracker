ee98ee75d3b5be17556549b605c11d243e0e1fde
Eric Noll <eanoll88@gmail.com>
2020-02-03 21:07:31 -0600

    route finalization

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
 router.post("/api/workouts", (req, res) => {
-    console.log("route: post/api/workouts");
     Workout.create({})
         .then(dbWorkout => {
             res.json(dbWorkout);
@@ -10,19 +10,11 @@ router.post("/api/workouts", (req, res) => {
             res.json(err);
         });
 });
-
-// add a new workout
-router.post('/api/workouts', (req, res) => {
-    Workout.create({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-})
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-    console.log("route: put/api/workouts/" + params.id);
     Workout.findByIdAndUpdate(
         params.id,
         { $push: { exercises: body } },
+        // "runValidators" will ensure new exercises meet our schema requirements
         { new: true, runValidators: true }
     )
         .then(dbWorkout => {
@@ -33,7 +25,6 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
         });
 });
 router.get("/api/workouts", (req, res) => {
-    console.log("route: get/api/workouts");
     Workout.find()
         .then(dbWorkouts => {
             res.json(dbWorkouts);
@@ -43,7 +34,6 @@ router.get("/api/workouts", (req, res) => {
         });
 });
 router.get("/api/workouts/range", (req, res) => {
-    console.log("route: get/api/workouts/range")
     Workout.find({}).limit(7)
         .then(dbWorkouts => {
             console.log(dbWorkouts)
@@ -54,7 +44,6 @@ router.get("/api/workouts/range", (req, res) => {
         });
 });
 router.delete("/api/workouts", ({ body }, res) => {
-    console.log("route: delete/api/workouts")
     Workout.findByIdAndDelete(body.id)
         .then(() => {
             res.json(true);
